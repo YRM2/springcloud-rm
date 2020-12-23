96aba6af4e1189a9488468546a9ff730023a9bee
YRM2 <yinruiming1219@163.com>
2020-12-24 00:08:49 +0800

    1、远端获取配置

--- service-one/src/main/java/com/springcloud/rm/serviceone/controller/Hello.java
+++ service-one/src/main/java/com/springcloud/rm/serviceone/controller/Hello.java
@@ -1,18 +1,29 @@
 package com.springcloud.rm.serviceone.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class Hello {
 
     @Value("${server.port}")
     String port;
 
+    @Value("${env}")
+    String logLevel;
+
     @GetMapping("/hello")
     public String Hello(){
         System.out.println(port);
         return port;
+    }
+
+    @GetMapping("/logLevel")
+    public String LogLevel(){
+        System.out.println(logLevel);
+        return logLevel;
     }
 }
